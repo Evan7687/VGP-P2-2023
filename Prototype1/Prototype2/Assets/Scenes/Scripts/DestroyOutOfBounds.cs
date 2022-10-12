c8dffea8045a1034bc3c0a63d025c09aff16acab
Evan7687 <113127273+Evan7687@users.noreply.github.com>
2022-10-12 09:16:23 -0700

    Fixed bug

--- Prototype1/Prototype2/Assets/Scenes/Scripts/DestroyOutOfBounds.cs
+++ Prototype1/Prototype2/Assets/Scenes/Scripts/DestroyOutOfBounds.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DestroyOutOfBounds : MonoBehaviour
 {
-    private float topBound
+    private float topBound;
     // Start is called before the first frame update
     void Start()
     {
