731132eb4e4d6b30d09a2b3054f358dac0511eab
Evan7687 <113127273+Evan7687@users.noreply.github.com>
2022-10-20 08:45:02 -0700

    Random spawn

--- Prototype1/Prototype2/Assets/Scenes/Scripts/SpawnManager.cs
+++ Prototype1/Prototype2/Assets/Scenes/Scripts/SpawnManager.cs
@@ -6,6 +6,8 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
     public int animalIndex;
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,10 +15,14 @@ public class SpawnManager : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void Update() 
+
     {
-        if (Input.GetKeyDown(KeyCode.S))    
+        if (Input.GetKeyDown(KeyCode.S))
         {
+            // Randomly generate animal indes and spawn position
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ); 
+            int animalIndex = Random.Range(0, animalPrefabs.Length);
             Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20), animalPrefabs[animalIndex].transform.rotation);
         }
         
