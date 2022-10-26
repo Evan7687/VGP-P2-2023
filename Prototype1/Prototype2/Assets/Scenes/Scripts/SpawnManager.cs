3ba9f0df474fe73a427604486a766008f975b667
Evan7687 <113127273+Evan7687@users.noreply.github.com>
2022-10-26 09:11:55 -0700

    Colision

--- Prototype1/Prototype2/Assets/Scenes/Scripts/SpawnManager.cs
+++ Prototype1/Prototype2/Assets/Scenes/Scripts/SpawnManager.cs
@@ -8,10 +8,13 @@ public class SpawnManager : MonoBehaviour
     public int animalIndex;
     private float spawnRangeX = 20;
     private float spawnPosZ = 20;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
@@ -20,11 +23,14 @@ public class SpawnManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.S))
         {
-            // Randomly generate animal indes and spawn position
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ); 
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+           SpawnRandomAnimal();
         }
         
+    }
+    void SpawnRandomAnimal(){
+         // Randomly generate animal indes and spawn position
+            int animalIndex = Random.Range(0, animalPrefabs.Length);
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ); 
+            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
     }
 }
