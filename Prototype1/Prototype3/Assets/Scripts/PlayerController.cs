30a1bf5970b98c292a52105f7b660017a23035d7
Evan7687 <113127273+Evan7687@users.noreply.github.com>
2022-12-09 09:15:30 -0800

    audio

--- Prototype1/Prototype3/Assets/Scripts/PlayerController.cs
+++ Prototype1/Prototype3/Assets/Scripts/PlayerController.cs
@@ -13,6 +13,9 @@ public class PlayerController : MonoBehaviour
     private Animator playerAnim;
     public ParticleSystem explosionParticle;
     public ParticleSystem dirtParticle;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+    private AudioSource playerAudio;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +23,7 @@ public class PlayerController : MonoBehaviour
         playerRb = GetComponent<Rigidbody>();
         Physics.gravity *= gravityModifier;
         playerAnim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -31,6 +35,7 @@ public class PlayerController : MonoBehaviour
             isOnGround = false;
             playerAnim.SetTrigger("Jump_trig");
             dirtParticle.Stop();
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
         }
         
     }
@@ -52,6 +57,7 @@ public class PlayerController : MonoBehaviour
                 playerAnim.SetInteger("DeathType_int", 1);
                 explosionParticle.Play();
                 dirtParticle.Stop();
+                playerAudio.PlayOneShot(crashSound, 1.0f);
             }
         }
 }
