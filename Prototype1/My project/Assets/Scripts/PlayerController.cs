66f32f327dd9fc258d39a3843fb1689276f3002a
Evan7687 <113127273+Evan7687@users.noreply.github.com>
2023-03-03 09:49:57 -0800

    Smash powerup

--- Prototype1/My project/Assets/Scripts/PlayerController.cs
+++ Prototype1/My project/Assets/Scripts/PlayerController.cs
@@ -33,6 +33,8 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(currentPowerUp + " and " + smashing);
+
         float forwardInput = Input.GetAxis("Vertical");
 
         playerRb.AddForce(focalPoint.transform.forward * forwardInput * speed);
@@ -43,6 +45,13 @@ public class PlayerController : MonoBehaviour
         {
             Debug.Log("fire");
             LaunchRockets();
+        }
+
+        if(currentPowerUp == PowerUpType.Rockets && Input.GetKeyDown(KeyCode.Space) && !smashing)
+        {
+            Debug.Log("Space was pressed");
+            smashing = true;
+            StartCoroutine(Smash());
         }
     }
 
@@ -97,6 +106,34 @@ public class PlayerController : MonoBehaviour
 
         //Store the y position before taking off
         floorY = transform.position.y;
+
+        //Calculate the amount of time we will go up
+        float jumpTime = Time.time + hangTime;
+
+        while(Time.time < jumpTime)
+        {
+            //move the player up while still keeping their x velocity
+            playerRb.velocity = new Vector2(playerRb.velocity.x, smashSpeed);
+            yield return null;
+        }
+
+        //Move the player down
+        while(transform.position.y > floorY)
+        {
+            playerRb.velocity = new Vector2(playerRb.velocity.x, -smashSpeed * 2);
+            yield return null;
+        }
+
+        //Cycle through all enemies
+        for(int i = 0; i < enemies.Length; i++)
+        {
+            //Apply an explosion force that originates from our position
+            if(enemies[i] != null)
+                enemies [i].GetComponent<Rigidbody>().AddExplosionForce(explosionForce, transform.position, explosionRadius, 0.0f, ForceMode.Impulse);
+        }
+
+        //We are no longer smashing, so set the boolean to false
+        smashing = false;
     }
 }   
 
--- Prototype1/My project/Assets/Scripts/SpawnManager.cs
+++ Prototype1/My project/Assets/Scripts/SpawnManager.cs
@@ -25,7 +25,6 @@ public class SpawnManager : MonoBehaviour
     void Update()
     {
         enemyCount = FindObjectsOfType<Enemy>().Length;
-
         if(enemyCount == 0)
         {
             waveNumber++;
