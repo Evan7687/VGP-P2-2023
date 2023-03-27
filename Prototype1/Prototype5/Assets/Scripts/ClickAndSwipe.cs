6e5b9c3dc82af95fae75816745c31bb3ebc6e2e8
Evan7687 <113127273+Evan7687@users.noreply.github.com>
2023-03-27 09:04:10 -0700

    Click And Swipe

---
+++ Prototype1/Prototype5/Assets/Scripts/ClickAndSwipe.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(TrailRenderer), typeof(BoxCollider))]
+
+public class ClickAndSwipe : MonoBehaviour
+{
+    //so we can access the GameManager Script and it's public variable and methods
+    private GameManager gameManager;
+    private Camera cam;
+    //Remember Vector3 gives us an x, y and z
+    private Vector3 mousePos;
+    private TrailRenderer trail;
+    private BoxCollider col;
+    //remember that bool is short for boolean (true/false variable)
+    private bool swiping = false;
+
+    void Awake()
+    {
+        cam = Camera.main;
+        trail = GetComponent<TrailRenderer>();
+        col = GetComponent<BoxCollider>();
+        trail.enabled = false;
+        col.enabled = false;
+
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameManager.isGameActive)
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                swiping = true;
+                UpdateComponents();
+            }
+            else if (Input.GetMouseButtonUp(0))
+            {
+                swiping = false;
+                UpdateComponents();
+            }
+            if (swiping)
+            {
+                UpdateMousePosition();
+            }
+        }
+    }
+
+    void UpdateMousePosition()
+    {
+        mousePos = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f));
+        transform.position = mousePos;
+    }
+
+    void UpdateComponents()
+    {
+        trail.enabled = swiping;
+        col.enabled = swiping;
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.GetComponent<Target>())
+        {
+            //Destroy the target
+        }
+    }
+}
