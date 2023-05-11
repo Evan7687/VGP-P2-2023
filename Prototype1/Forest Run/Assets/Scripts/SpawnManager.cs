9e2ba556a97baaf29d1101abbba9383802891a2d
Evan7687 <113127273+Evan7687@users.noreply.github.com>
2023-05-11 09:13:00 -0700

    Spawn Animals

--- Prototype1/Forest Run/Assets/Scripts/SpawnManager.cs
+++ Prototype1/Forest Run/Assets/Scripts/SpawnManager.cs
@@ -8,7 +8,7 @@ public class SpawnManager : MonoBehaviour
     public int animalIndex;
     private float spawnRangeX = 20;
     private float spawnPosZ = 20;
-    private float startDelay = 2;
+    private float startDelay = 3;
     private float spawnInterval = 1.5f;
 
     // Start is called before the first frame update
@@ -18,20 +18,19 @@ public class SpawnManager : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update() 
-
+    void Update()
     {
         if (Input.GetKeyDown(KeyCode.S))
         {
-           SpawnRandomAnimal();
+            SpawnRandomAnimal();
         }
         
     }
     void SpawnRandomAnimal()
     {
-         // Randomly generate animal indes and spawn position
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ); 
-            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+        //Randomly generate animal indes and spawn positions
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
     }
 }
